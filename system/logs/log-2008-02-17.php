d022ad19a2036589a9f5af915f9ac5bb29cec5e4
Evan Walsh <evan@adsl-99-130-217-25.dsl.ipltin.sbcglobal.net>
2008-04-16 15:33:53 -0400

    Removed system/replaced with core

--- system/logs/log-2008-02-17.php
+++
@@ -1,4 +0,0 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2008-02-17 23:08:34 --> Severity: Notice  --> Undefined variable: fields /Users/evan/Sites/htdocs/klei/system/application/models/main.php 215
-ERROR - 2008-02-17 23:11:00 --> 404 Page Not Found --> 
--- system/logs/log-2008-02-21.php
+++
@@ -1,3 +0,0 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2008-02-21 22:19:40 --> Severity: Notice  --> Undefined variable: fields /Users/evan/Sites/htdocs/klei/system/application/models/main.php 221
