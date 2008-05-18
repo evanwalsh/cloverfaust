92e674a45ef426be3b8a172aad76357f8a141cb6
Evan Walsh <evan@seraph.local>
2008-05-17 20:29:26 -0400

    Fixed theme issue

--- views/themes/blatt/edit.php
+++
@@ -1,8 +0,0 @@
-<?=form_open("edit/post/".$post->id,array("id" => "editform"))?>
-	<p><?=form_label("Your post's body")?></p>
-	<p><?=form_textarea("body",$post->body)?></p>
-	<p><?=form_label("&nbsp;")?></p>
-	<p><?=form_submit("","Save")?><?=form_submit("","Cancel",'onClick="window.location=\''.base_url()."show/topic/".$post->url.'\';return false;"')?></p>
-	<?=form_hidden("forum",$post->forum)?>
-	<?=form_hidden("post",$post->url)?>
-<?=form_close()?>
